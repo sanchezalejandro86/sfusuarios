223943b4c46fe265508273510820bc0ac9d42ca6
Alejandro Sanchez <sanchezalejandro86@gmail.com>
2017-03-15 14:56:33 -0300

    config con parametros del cliente-oauth

---
+++ cliente-oauth/config.php
@@ -0,0 +1,6 @@
+<?php
+
+$host = 'http://usuarios.hc.com/app_dev.php';
+$client_id = '4_4ag4h45sgrggg80cwcsk0c88owc80ck8c0ccsok4sso0w4os88';
+$secret = 'i8t0bdky3mgcc0wkwco4s4cggok0o8owo0ssgo8o4gw04480o';
+$redirect_uri = 'http://cliente-oauth/callback.php';
